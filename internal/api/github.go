4d0495272b18bfff46193aa4c7f3b2ba9d1a4b29
Chongyi Zheng <harry@harryzheng.com>
2022-04-07 22:47:48 -0400

    Skip token request if already exists

--- internal/api/github.go
+++ internal/api/github.go
@@ -11,43 +11,52 @@ import (
 func Auth() error {
 	storage.InitDefault()
 
-	fmt.Println("Generating GitHub OAuth token for CLI...")
-	flow := &oauth.Flow{
-		Host:     oauth.GitHubHost("https://github.com"),
-		ClientID: os.Getenv("GITHUB_CLIENT_ID"),
-		Scopes:   []string{"repo"},
+	token := storage.GetToken(storage.TokenKindCLI)
+	if token != nil {
+		fmt.Println("You already have a CLI token, skipping...")
+	} else {
+		fmt.Println("Generating GitHub OAuth token for CLI...")
+		flow := &oauth.Flow{
+			Host:     oauth.GitHubHost("https://github.com"),
+			ClientID: os.Getenv("GITHUB_CLIENT_ID"),
+			Scopes:   []string{"repo"},
+		}
+
+		accessToken, err := flow.DetectFlow()
+		if err != nil {
+			return err
+		}
+
+		err = storage.StoreToken(storage.TokenKindCLI, accessToken)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Successfully authenticated with GitHub")
 	}
 
-	accessToken, err := flow.DetectFlow()
-	if err != nil {
-		return err
+	token = storage.GetToken(storage.TokenKindGitHubGitea)
+	if token != nil {
+		fmt.Println("You already have a GitHub OAuth token for Gitea, skipping...")
+	} else {
+		fmt.Println("Generating GitHub OAuth token for Gitea...")
+		flow := &oauth.Flow{
+			Host:     oauth.GitHubHost("https://github.com"),
+			ClientID: os.Getenv("GITHUB_CLIENT_ID"),
+			Scopes:   []string{"repo"},
+		}
+
+		accessToken, err := flow.DetectFlow()
+		if err != nil {
+			return err
+		}
+
+		err = storage.StoreToken(storage.TokenKindGitHubGitea, accessToken)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Successfully authenticated with GitHub")
 	}
-
-	err = storage.StoreToken(storage.TokenKindCLI, accessToken)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Successfully authenticated with GitHub")
-
-	fmt.Println("Generating GitHub OAuth token for Gitea...")
-	flow = &oauth.Flow{
-		Host:     oauth.GitHubHost("https://github.com"),
-		ClientID: os.Getenv("GITHUB_CLIENT_ID"),
-		Scopes:   []string{"repo"},
-	}
-
-	accessToken, err = flow.DetectFlow()
-	if err != nil {
-		return err
-	}
-
-	err = storage.StoreToken(storage.TokenKindGitHubGitea, accessToken)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Successfully authenticated with GitHub")
 
 	return nil
 }
