6c15d500735b81222f928b76be2bce55f3de682c
harryzcy <harry@harryzheng.com>
2022-11-29 01:40:49 -0500

    Support setting up gitea push mirrors

--- cmd/gitea.go
+++ cmd/gitea.go
@@ -17,6 +17,7 @@ var giteaCmd = &cobra.Command{
 
 var giteaMigrateCmd = &cobra.Command{
 	Use:   "migrate",
+	Args:  cobra.ExactArgs(1),
 	Short: "Migrate a repository to Gitea",
 	Run: func(cmd *cobra.Command, args []string) {
 		uri, err := argutil.GetURI(args)
@@ -44,17 +45,10 @@ var giteaMigrateCmd = &cobra.Command{
 }
 
 var giteaMirrorCmd = &cobra.Command{
-	Use:   "mirror",
+	Use:   "mirror url",
+	Args:  cobra.ExactArgs(1),
 	Short: "Mirror a repository to Gitea",
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			fmt.Println("Please specify the repository URL")
-			os.Exit(1)
-		} else if len(args) > 1 {
-			fmt.Println("Please specify only one repository URL")
-			os.Exit(1)
-		}
-
 		uri := args[0]
 		options := api.MigrateRepoOptions{
 			GitURI: uri,
@@ -64,6 +58,24 @@ var giteaMirrorCmd = &cobra.Command{
 		options.Name, _ = cmd.Flags().GetString("name")
 
 		err := api.MigrateRepo(options)
+		if err != nil {
+			os.Exit(1)
+		}
+	},
+}
+
+var giteaPushMirrorCmd = &cobra.Command{
+	Use:     "pushmirror",
+	Args:    cobra.ExactArgs(2),
+	Aliases: []string{"pm"},
+	Short:   "Setup a push mirror to a repository",
+	Run: func(cmd *cobra.Command, args []string) {
+		options := api.SetupPushMirrorOptions{
+			UsernameRepo: args[0],
+			GitURI:       args[1],
+		}
+
+		err := api.SetupPushMirror(options)
 		if err != nil {
 			os.Exit(1)
 		}
@@ -81,4 +93,6 @@ func init() {
 	giteaCmd.AddCommand(giteaMirrorCmd)
 	giteaMirrorCmd.Flags().BoolP("private", "", false, "create the private repository")
 	giteaMirrorCmd.Flags().StringP("name", "n", "", "name of the repository")
+
+	giteaCmd.AddCommand(giteaPushMirrorCmd)
 }
