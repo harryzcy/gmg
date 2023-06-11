f7fa76b290fabfcce35a5ad055ad58d93bed780e
harryzcy <harry@harryzheng.com>
2023-06-11 17:52:13 +0000

    Fix update

--- internal/api/github.go
+++ internal/api/github.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/cli/oauth"
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 	"go.zcy.dev/gmg/internal/storage"
 	"golang.org/x/oauth2"
 )
