31f1a6c896f6c47faff6b8421317cb0a0ff11389
Chongyi Zheng <git@zcy.dev>
2023-09-05 23:14:39 +0000

    Use correct major version

--- internal/api/github.go
+++ internal/api/github.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/cli/oauth"
-	"github.com/google/go-github/v54/github"
+	"github.com/google/go-github/v55/github"
 	"go.zcy.dev/gmg/internal/storage"
 	"golang.org/x/oauth2"
 )
